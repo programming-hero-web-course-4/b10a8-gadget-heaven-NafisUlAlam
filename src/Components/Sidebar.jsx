ac5b992203fe1293712a07f16ee27578ae4ef4af
Nafis <ahmednafis74@gmail.com>
2024-11-06 00:33:09 +0600

    sidebar buttons made good

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,14 +4,14 @@ const Sidebar = () => {
   const categeories = useLoaderData();
   //console.log(categeories);
   return (
-    <div className="flex flex-wrap md:flex-col gap-4  p-4 mb-4 md:mb-0 md:w-[20%]">
+    <div className="flex flex-wrap md:flex-col gap-4  p-4 mb-4 md:mb-0 md:w-[20%] flex-grow-0 flex-shrink-0">
       {categeories &&
         categeories.map((category) => (
           <NavLink
             className={({ isActive }) =>
               isActive
-                ? "bg-[#9538E2] text-white rounded-full px-4 py-2"
-                : "bg-white text-black px-4 py-2"
+                ? "bg-[#9538E2] text-white rounded-full px-4 py-2 hover:bg-[#7327b1] hover:text-white text-center shadow-lg"
+                : "bg-white text-black px-4 py-2 hover:bg-[#7327b1] hover:text-white rounded-full"
             }
             key={category.id}
             to={`/category/${category.name}`}
